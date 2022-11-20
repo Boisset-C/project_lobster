66c63cddb60b714e204a66c6281b34fd096d4dea
Cosme <cosme@Vitals-MacBook-Pro-2.local>
2022-11-20 01:15:52 -0500

    Removed unused imports.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,5 @@
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import "./home.css";
-import Button from "react-bootstrap/Button";
-import { useWindowSize } from "../hooks/useWindowSize";
-import heroImage from "../assets/heroImage.jpg";
-import heroImageWeb from "../assets/heroImageWeb.jpg";
+import Container from "react-bootstrap/Container";
 import Hero from "../component/shelter-review-components/Hero";
 
 function Home() {
